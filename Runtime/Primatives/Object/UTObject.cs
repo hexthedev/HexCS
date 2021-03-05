84f71e8f1eb3341b5a08228da795f99f2397eeda
james <jamesmccafferty@live.ca>
2021-03-05 12:51:59 -0500

    Added UTObject

---
+++ Runtime/Primatives/Object/UTObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexCS.Core
+{
+    public static class UTObject
+    {
+        public static bool TryAs<T>(this object ob, out T val)
+        {
+            if (!(ob is T))
+            {
+                val = default;
+                return false;
+            }
+
+            val = (T)ob;
+            return true;
+        }
+    }
+}
