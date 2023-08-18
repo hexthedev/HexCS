5a7e21cdd58d60d9006210b42aa4108a6fad6433
James <jamesmccafferty@live.ca>
2023-08-18 14:24:34 -0400

    Added Dictionary utilities

---
+++ Runtime/DataStructures/Dictionary/UTDictionary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace HexCS.Core
+{
+    public static class UTDictionary
+    {
+        /// <summary>
+        /// Adds all values from other to target. Overrides repeat keys.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="other"></param>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        public static void Combine<T1, T2>(this Dictionary<T1, T2> target, Dictionary<T1, T2> other)
+        {
+            foreach (T1 otherKey in other.Keys)
+                target[otherKey] = other[otherKey];
+        }
+    }
+}
